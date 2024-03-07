d9cc8b260b59579adfd32f6ab686ff7533009c74
Nicollas-Trovao <137838083+Nicollas-Trovao@users.noreply.github.com>
2024-03-06 21:46:19 -0300

    Update and rename Lista.java to Principal.java

--- Principal.java
+++ Principal.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Lista {
+public class Pricipal {
 
     public static void main(String[] args) {
         ArrayList<Pessoa> pessoas = new ArrayList<>();
