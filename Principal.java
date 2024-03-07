e3e787232e0138d133c8a75364570a470a9c7b9c
Nicollas-Trovao <137838083+Nicollas-Trovao@users.noreply.github.com>
2024-03-06 21:46:53 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Pricipal {
+public class Principal {
 
     public static void main(String[] args) {
         ArrayList<Pessoa> pessoas = new ArrayList<>();
